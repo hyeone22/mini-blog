72e7b6bb578ac0320f7340a80073c640a7cd9f2e
hyeone22 <127266003+hyeone22@users.noreply.github.com>
2023-10-01 17:47:43 +0900

    first commit

---
+++ src/components/list/CommentListItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function CommentListItem(props) {
+
+  const Wrapper = styled.div`
+      width: calc(100% - 32px);
+      padding: 8px 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      border: 1px solid grey;
+      border-radius: 8px;
+      cursor: pointer;
+      background: white;
+        :hover {
+          background: lightgrey;
+        }
+  `;
+  
+  const ContenteText = styled.p`
+      font-size: 16px;
+      white-space: pre-wrap;  
+  `
+
+  const { comment } = props; 
+  return (
+      <Wrapper>
+        <ContenteText>{comment.content}</ContenteText>
+      </Wrapper>
+  )
+}
+
+export default CommentListItem
